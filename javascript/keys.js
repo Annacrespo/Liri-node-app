9a860a4230204e285135224b419263b0794731f7
annacrespo12 <annacrespo12@gmail.com>
2017-09-11 16:51:35 -0700

    liri.js init commit

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'RcMNMonUljUoz2iDef0clFcuh',
+  consumer_secret: 'Zhk1JwWArsMreiZYK8oFQMFA7jXtc9I0BYXNDUyHQ9uUOxwbrm',
+  access_token_key: '907380555192270848-GB3QQAIEtqTC8HUzjRldFuErDwBoH3d',
+  access_token_secret: 'NdDn0zwB7ntkT94lJVMmrUIZNMTuajG3yyGc6XaIQE52v',
 }
 
 module.exports = twitterKeys;
