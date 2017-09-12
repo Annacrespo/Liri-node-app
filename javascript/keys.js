dfa944445578fb7f61511642c100c41ee997a6f1
annacrespo12 <annacrespo12@gmail.com>
2017-09-11 22:19:15 -0700

    twitter key working

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,10 +1,12 @@
 console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'RcMNMonUljUoz2iDef0clFcuh',
-  consumer_secret: 'Zhk1JwWArsMreiZYK8oFQMFA7jXtc9I0BYXNDUyHQ9uUOxwbrm',
-  access_token_key: '907380555192270848-GB3QQAIEtqTC8HUzjRldFuErDwBoH3d',
-  access_token_secret: 'NdDn0zwB7ntkT94lJVMmrUIZNMTuajG3yyGc6XaIQE52v',
-}
 
-module.exports = twitterKeys;
+var twitterKeys = {
+    consumer_key: 'RcMNMonUljUoz2iDef0clFcuh',
+    consumer_secret: 'Zhk1JwWArsMreiZYK8oFQMFA7jXtc9I0BYXNDUyHQ9uUOxwbrm',
+    access_token_key: '907380555192270848-GB3QQAIEtqTC8HUzjRldFuErDwBoH3d',
+    access_token_secret: 'NdDn0zwB7ntkT94lJVMmrUIZNMTuajG3yyGc6XaIQE52v',
+  }
+  
+
+  module.exports.twitterKeys = twitterKeys;
---
+++ javascript/liri.js
@@ -0,0 +1,21 @@
+//twitter key
+const keys = require("./keys");
+var Twitter = require("twitter");
+
+var fs = require("fs");
+
+
+var client = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret,
+});
+
+var params = {screen_name: 'opsercanna'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
