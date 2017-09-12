2d6f3a011255399f2d43023fdfa231713a4beee2
annacrespo12 <annacrespo12@gmail.com>
2017-09-11 23:14:27 -0700

    spotify keys

--- javascript/keys.js
+++ javascript/keys.js
@@ -8,5 +8,10 @@ var twitterKeys = {
     access_token_secret: 'NdDn0zwB7ntkT94lJVMmrUIZNMTuajG3yyGc6XaIQE52v',
   }
   
-
+ var spotifyKeys= {
+   id: '852f84f776f84090b384a750e3cbf69b',
+   secret: '1e30ad4dda8a494f8fa5eae17c616aef'
+ }
+ 
   module.exports.twitterKeys = twitterKeys;
+  module.exports.spotifyKeys = spotifyKeys;
--- javascript/liri.js
+++ javascript/liri.js
@@ -1,6 +1,8 @@
-//twitter key
+//twitter 
 const keys = require("./keys");
+
 var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
 
 var fs = require("fs");
 
@@ -16,6 +18,64 @@ var params = {screen_name: 'opsercanna'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
+    for (var i=0; i<tweets.length; i++) {
+        console.log(tweets[i].text);
+    }
   }
+ 
+  
 });
 
+//spotify
+var spotify = new Spotify({
+	id: keys.spotifyKeys.id,
+	secret: keys.spotifyKeys.secret
+});
+
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+        console.log(data);
+    // Do something with 'data'
+});
+
+// { tracks:
+//     { href: 'https://api.spotify.com/v1/search?query=dancing+in+the+moonlight&type=track&offset=0&limit=20',
+//       items:
+//        [ [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object],
+//          [Object] ],
+//       limit: 20,
+//       next: 'https://api.spotify.com/v1/search?query=dancing+in+the+moonlight&type=track&offset=20&limit=20',
+//       offset: 0,
+//       previous: null,
+//       total: 868 } }
+
+
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+   
+//   console.log(data); 
+//   });
